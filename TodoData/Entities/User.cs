2bf22a74b44caa5aeafa9ddf2265de61e97c07a3
Ankita Khurana <ankita0096@gmail.com>
2018-09-19 12:09:27 +0530

    .net Framework for data

--- TodoData/Entities/User.cs
+++ TodoData/Entities/User.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Data.Entities
+namespace TodoData.Entities
 {
     public class User
     {
---
+++ TodoData/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TodoData.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TodoData.Entities.TodoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TodoData.Entities.TodoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
