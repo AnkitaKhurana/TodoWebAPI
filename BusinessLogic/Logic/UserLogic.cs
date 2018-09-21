412d5e30061fdf7c98092b20bb8ff6acd090126c
Ankita Khurana <ankita0096@gmail.com>
2018-09-21 11:24:56 +0530

    User Logic

---
+++ BusinessLogic/Logic/UserLogic.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.UserData;
+using Shared.DTOs;
+using Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Logic
+{
+    public class UserLogic
+    {
+        UserData userData = new UserData();
+        public bool Register(UserDTO user)
+        {
+            try
+            {
+                return userData.AddUser(user);               
+            }
+            catch (UserAlreadyExists)
+            {
+                throw new UserAlreadyExists();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public UserDTO Login(UserDTO user)
+        {
+            try
+            {
+                if (userData.FindUserByCredential(user.UserName, user.Password)!=null)
+                {
+                    return user;
+                }
+                return null;
+            }
+            catch (NoSuchUserExists)
+            {
+                throw new NoSuchUserExists();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
